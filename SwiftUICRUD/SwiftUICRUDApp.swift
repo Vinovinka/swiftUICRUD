269c67c3f53ba949e062aa66107407e3255a7753
Victoria Vinogradova <vinovinka@yandex.ru>
2022-10-04 16:41:19 +0300

    Initial Commit

---
+++ SwiftUICRUD/SwiftUICRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICRUDApp.swift
+//  SwiftUICRUD
+//
+//  Created by Виктория Виноградова on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
